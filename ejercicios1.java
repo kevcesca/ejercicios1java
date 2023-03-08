983390f6c754fa34da946c93afd392aeaad1bf68
kevokl <lacartax@gmail.com>
2023-03-08 14:11:34 -0600

    first commit

---
+++ ejercicios1.java
@@ -0,0 +1,15 @@
+package practicaJava;
+
+public class ClaseMain {
+	
+	static void primerMetodo(int x) {
+		System.out.println(x);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		primerMetodo(7);
+	}
+
+}
